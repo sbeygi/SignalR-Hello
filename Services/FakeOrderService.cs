20a3090fb2027bab574ada9305748140441db7eb
S. Beygi <mail.sbeygi@gmail.com>
2018-11-30 10:29:26 -0500

    Implement a FakeOrderService and host it as a service on startup

---
+++ Services/FakeOrderService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Hosting;
+using SignalRChat.Hubs;
+
+namespace SignalR_Hello
+{
+	public class FakeOrderService : IHostedService
+	{
+		public IHubContext<PushHub> HubContext { get; }
+		private Timer timer;
+
+		public FakeOrderService(IHubContext<PushHub> hubContext)
+		{
+			HubContext = hubContext;
+		}
+
+		public async Task StartAsync(CancellationToken cancellationToken)
+		{
+			timer = new Timer(delegate (object state)
+			{
+				try
+				{
+					HubContext.Clients.Groups("ShippingHub").SendAsync("NewPushMessage", WebUtility.HtmlEncode($"{DateTime.Now}: Your order just got delivered."));
+				}
+				catch { }
+			}, null, TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(30));
+		}
+
+		public async Task StopAsync(CancellationToken cancellationToken)
+		{
+			timer.Dispose();
+		}
+	}
+}
--- Startup.cs
+++ Startup.cs
@@ -25,13 +25,18 @@ namespace SignalR_Hello
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+	        
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 	        services.AddSignalR();
+
+	        services.AddHostedService<FakeOrderService>();
 		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+	        app.UseStaticFiles();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
