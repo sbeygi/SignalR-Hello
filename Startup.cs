eda239a3b63ad92b587a0065478354e1c3ec937a
S. Beygi <mail.sbeygi@gmail.com>
2018-11-30 08:09:42 -0500

    Configure SignalR in startup.cs

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SignalRChat.Hubs;
 
 namespace SignalR_Hello
 {
@@ -25,7 +26,8 @@ namespace SignalR_Hello
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-        }
+	        services.AddSignalR();
+		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -35,7 +37,12 @@ namespace SignalR_Hello
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+	        app.UseSignalR(routes =>
+	        {
+		        routes.MapHub<PushHub>("/pushHub");
+	        });
+
+			app.UseMvc();
         }
     }
 }
